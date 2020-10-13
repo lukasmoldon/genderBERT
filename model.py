6ec7a874529f5ceb9219b371f6ab1780a63f82c1
Timur Bachschi <timurbachschi@hotmail.de>
2020-10-13 14:24:40 +0200

    Refactoring

--- model.py
+++ model.py
@@ -28,6 +28,7 @@ PRELOAD_MODEL = None
 VAL_ROWS = 100000
 LOAD_EMBEDDINGS = None
 NUM_ROWS_TRAIN = 1000000
+MODEL_TYPE = "bert"
 # --------------------------------
 
 
@@ -51,6 +52,7 @@ def set_config():
             VAL_ROWS = config[mode]["VAL_ROWS"]
             LOAD_EMBEDDINGS = config[mode]["LOAD_EMBEDDINGS"]
             NUM_ROWS_TRAIN = config[mode]["NUM_ROWS_TRAIN"]
+            MODEL_TYPE = config[mode]["MODEL_TYPE"]
     elif len(sys.argv) == 1:
         logging.warning("Config number missing!")
     else:
@@ -62,6 +64,87 @@ def flat_accuracy(predicitions, labels):
     pred_flat = np.argmax(predicitions, axis=1).flatten()
     labels_flat = labels.flatten()
     return np.sum(pred_flat == labels_flat) / len(labels_flat)
+
+
+def create_dataloader(dataframe, batch_size):
+    logging.info("Create tensors ...")
+    inputs = list(dataframe["ReviewText"])
+    labels = list(dataframe["Gender"])
+    mask = list(dataframe["att_mask"])
+
+    X_tens = torch.tensor(inputs)
+    y_tens = torch.tensor(labels)
+    mask_tens = torch.tensor(mask)
+
+    # Create TensorDataset/Dataloader for faster training
+    logging.info("Create DataLoader ...")
+    data = TensorDataset(X_tens, y_tens, mask_tens)
+    sampler = RandomSampler(data)
+    dataloader = DataLoader(data, sampler=sampler, batch_size=batch_size)
+    return dataloader
+
+
+def train_epoch(model, data_loader, optimizer, device, scheduler):
+    # TODO: How exactly is the loss function specified?
+    logging.info("Training ...")
+    total_loss = 0
+    model.train()
+    tmp_train_acc, steps = 0, 0
+    for step, batch in enumerate(data_loader):
+        b_input_ids = batch[0].to(device)
+        b_labels = batch[1].to(device)
+        b_input_mask = batch[2].to(device)
+        model.zero_grad()
+        # batch[0]: ids, [1]:masks, [2]:labels
+        outputs = model(b_input_ids,
+                        token_type_ids=None,
+                        labels=b_labels,
+                        attention_mask=b_input_mask)
+        loss = outputs[0]
+        total_loss += loss.item()
+        loss.backward()
+        # Clip norm of gradients to 1.0
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+        optimizer.step()
+        scheduler.step()
+        logits = outputs[1].to("cpu")
+        label_ids = b_labels.to("cpu")
+        _, predicted = torch.max(logits, 1)
+        tmp_train_acc += (predicted == label_ids).sum().item()
+        steps += b_labels.size(0)
+    avg_train_loss = total_loss / len(data_loader)
+    accuracy = tmp_train_acc/steps
+    logging.info("Accuracy: {0:.2f}".format(accuracy))
+    logging.info("Average train loss: {0:.2f}".format(avg_train_loss))
+    return avg_train_loss, accuracy
+
+def eval_model(model, data_loader, device):
+    # TODO: Add loss
+    logging.info("-" * 30)
+    logging.info("Start validation ...")
+    model.eval()
+    tmp_eval_acc, steps = 0, 0
+    for batch in data_loader:
+        b_input_ids = batch[0].to(device)
+        b_labels = batch[1].to(device)
+        b_input_mask = batch[2].to(device)
+        with torch.no_grad():
+            outputs = model(b_input_ids,
+                            token_type_ids=None,
+                            attention_mask=b_input_mask)  
+            logits = outputs[0].to("cpu")
+            label_ids = b_labels.to("cpu")
+            _, predicted = torch.max(logits.data, 1)
+            tmp_eval_acc += (predicted == label_ids).sum().item()
+            steps += b_labels.size(0)
+    accuracy = tmp_eval_acc/steps
+    logging.info("Accuracy: {0:.2f}".format(accuracy))
+    return accuracy
+
+def test_model(model, data_loader, device):
+    # TODO
+    pass    
+        
 
 def train_model(epochs, learning_rate, batch_size, max_tokencount=510, truncating_method="head", model_train=True, save_model=True, preload_model=None, val_rows=None, load_embeddings=None, num_rows_train=None,
                 return_model=False, return_stats=False):
@@ -113,39 +196,18 @@ def train_model(epochs, learning_rate, batch_size, max_tokencount=510, truncatin
     else:
         logging.info("Tokenize train/validation data ...")
         if num_rows_train is not None:
-            embeddings = prepare_data("../datasets/amazon/User_level_train.csv", True, num_rows=num_rows_train, max_tokencount=max_tokencount, truncating_method=truncating_method)
+            train_df = prepare_data("../datasets/amazon/User_level_train.csv", True, num_rows=num_rows_train, max_tokencount=max_tokencount, truncating_method=truncating_method)
         else:
-            embeddings = prepare_data("../datasets/amazon/User_level_train.csv", True, max_tokencount=max_tokencount)
+            train_df = prepare_data("../datasets/amazon/User_level_train.csv", True, max_tokencount=max_tokencount)
         if val_rows is not None:
-            val_data = prepare_data("../datasets/amazon/User_level_validation.csv", True, num_rows=val_rows, max_tokencount=max_tokencount, truncating_method=truncating_method)
+            val_df = prepare_data("../datasets/amazon/User_level_validation.csv", True, num_rows=val_rows, max_tokencount=max_tokencount, truncating_method=truncating_method)
         else:
-            val_data = prepare_data("../datasets/amazon/User_level_validation.csv", True, max_tokencount=max_tokencount)
+            val = prepare_data("../datasets/amazon/User_level_validation.csv", True, max_tokencount=max_tokencount)
 
     # Create tensors from loaded data
     logging.info("Create tensors ...")
-    inputs = list(embeddings["ReviewText"])
-    labels = list(embeddings["Gender"])
-    mask = list(embeddings["att_mask"])
-    val_inputs = list(val_data["ReviewText"])
-    val_labels = list(val_data["Gender"])
-    val_mask = list(val_data["att_mask"])
-
-    X_train = torch.tensor(inputs)
-    X_val = torch.tensor(val_inputs)
-    y_train = torch.tensor(labels)
-    y_val = torch.tensor(val_labels)
-    train_mask = torch.tensor(mask)
-    validation_mask = torch.tensor(val_mask)
-
-    # Create TensorDataset/Dataloader for faster training
-    logging.info("Create DataLoader ...")
-    train_data = TensorDataset(X_train, y_train, train_mask)
-    train_sampler = RandomSampler(train_data)
-    train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=batch_size)
-
-    validation_data = TensorDataset(X_val, y_val, validation_mask)
-    validation_sampler = RandomSampler(validation_data)
-    validation_dataloader = DataLoader(validation_data, sampler=validation_sampler, batch_size=batch_size)
+    train_dataloader = create_dataloader(train_df, batch_size)
+    val_dataloader = create_dataloader(val_df, batch_size)
 
     # Create/load model
     if preload_model is None:
@@ -190,57 +252,21 @@ def train_model(epochs, learning_rate, batch_size, max_tokencount=510, truncatin
     torch.cuda.manual_seed_all(seed_val)
     loss_values = []
     stats = {}
+
+    # TODO: Save best model
     for epoch_i in range(epochs):
         logging.info("-------- Epoch {} / {} --------".format(epoch_i + 1, epochs))
         if model_train:
-            logging.info("Training ...")
-            total_loss = 0
-            model.train()
-            for step, batch in enumerate(train_dataloader):
-                b_input_ids = batch[0].to(device)
-                b_labels = batch[1].to(device)
-                b_input_mask = batch[2].to(device)
-                model.zero_grad()
-                # batch[0]: ids, [1]:masks, [2]:labels
-                outputs = model(b_input_ids,
-                                token_type_ids=None,
-                                labels=b_labels,
-                                attention_mask=b_input_mask)
-                loss = outputs[0]
-                total_loss += loss.item()
-                loss.backward()
-                # Clip norm of gradients to 1.0
-                torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
-                optimizer.step()
-                scheduler.step()
-            avg_train_loss = total_loss / len(train_dataloader)
-            loss_values.append(avg_train_loss)
-            logging.info("Average train loss: {0:.2f}".format(avg_train_loss))
-            stats[str(epoch_i)] = {"loss": avg_train_loss}
+            loss, acc = train_epoch(model, train_dataloader, optimizer, device, scheduler)
+            stats[str(epoch_i)] = {"loss": loss, "accuracy": acc}
             if save_model:
                 logging.info("Save model...")
-                # TODO: Use Tempfile instead of this mess of a name
                 model.save_pretrained("genderBERT_epoch_{}_V4".format(epoch_i))
         # ---VALIDATION--- 
         if val_rows is not None:
-            logging.info("Start validation ...")
-            model.eval()
-            tmp_eval_acc, steps = 0, 0
-            for batch in validation_dataloader:
-                b_input_ids = batch[0].to(device)
-                b_labels = batch[1].to(device)
-                b_input_mask = batch[2].to(device)
-                with torch.no_grad():
-                    outputs = model(b_input_ids,
-                                    token_type_ids=None,
-                                    attention_mask=b_input_mask)  
-                logits = outputs[0].to("cpu")
-                label_ids = b_labels.to("cpu")
-                _, predicted = torch.max(logits.data, 1)
-                tmp_eval_acc += (predicted == label_ids).sum().item()
-                steps += b_labels.size(0)
-            logging.info("Accuracy: {0:.2f}".format(tmp_eval_acc/steps))
-            stats[str(epoch_i)]["accuracy"] = tmp_eval_acc/steps
+            acc = eval_model(model, val_dataloader, device)
+            stats[str(epoch_i)]["accuracy"] = acc
+
     logging.info("Training complete!")
     log_endtime = datetime.datetime.now()
     log_runtime = (log_endtime - log_starttime)
@@ -255,5 +281,5 @@ def train_model(epochs, learning_rate, batch_size, max_tokencount=510, truncatin
         if return_model:
             return model
 
-# set_config()
-# train_model(EPOCHS, LEARNING_RATE, BATCH_SIZE, MAX_TOKENCOUNT, TRUNCATING_METHOD, MODEL_TRAIN, SAVE_MODEL, PRELOAD_MODEL, VAL_ROWS, LOAD_EMBEDDINGS, NUM_ROWS_TRAIN)
+set_config()
+train_model(EPOCHS, LEARNING_RATE, BATCH_SIZE, MAX_TOKENCOUNT, TRUNCATING_METHOD, MODEL_TRAIN, SAVE_MODEL, PRELOAD_MODEL, VAL_ROWS, LOAD_EMBEDDINGS, NUM_ROWS_TRAIN)
